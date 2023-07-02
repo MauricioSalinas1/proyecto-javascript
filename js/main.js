4a1940b157f712e691763aa7e2bc0e5d61733628
Mauricio Salinas <mauriciosalinas0703@gmail.com>
2023-07-02 13:05:57 -0300

    Agregado de un ciclo

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 function greet(nombre) {
     console.log(userName);
-    alert('¡Hola ' + userName + '! Quiero jugar un piedra, papel o tijeras con vos. ¿Crees que me vas a ganar?');
+    alert('¡Hola ' + userName + '! Quiero jugar un piedra, papel o tijeras contigo. ¿Crees que me vas a ganar?');
 }
 
 let userName = prompt('Ingrese su nombre');
-
 greet();
 
-let userSelect = prompt('¡Por favor escribe tu elección! "Piedra", "Papel" o "Tijera"');
+for (let i = 0; i < 5; i++) {
+    let userSelect = prompt('¡Por favor, escribe tu elección! "Piedra", "Papel" o "Tijera"');
 
-if (userSelect == 'Piedra') {
-    alert('¡Me ganaste ' + userName + '! Yo he elegido Tijera.');
-} else if (userSelect == 'Papel') {
-    alert('¡Hemos empatado ' + userName + '! Yo tambien he elegido Papel.');
-} else if (userSelect == 'Tijera') {
-    alert('¡Te gané ' + userName + '! Yo he elegido Piedra.');
-} else {
-    alert(userName + ' "' + userSelect + '" ' + 'no es un elemento del piedra papel o tijeras. Por favor escribe "Piedra", "Papel" o "Tijeras".');
+    if (userSelect === 'Piedra') {
+        alert('¡Me ganaste ' + userName + '! Yo he elegido Tijera.');
+    } else if (userSelect === 'Papel') {
+        alert('¡Hemos empatado ' + userName + '! Yo también he elegido Papel.');
+    } else if (userSelect === 'Tijera') {
+        alert('¡Te gané ' + userName + '! Yo he elegido Piedra.');
+    } else {
+        alert(userName + ' "' + userSelect + '" ' + 'no es un elemento válido de piedra, papel o tijeras. Por favor, escribe "Piedra", "Papel" o "Tijera".');
+    }
 }
