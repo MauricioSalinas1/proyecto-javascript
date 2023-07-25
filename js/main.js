d6d51f96c961409fac25688b35400373c5d8523a
Mauricio Salinas <mauriciosalinas0703@gmail.com>
2023-07-24 21:01:43 -0300

    Cambiado todo el formato para la segunda entrega

--- js/main.js
+++ js/main.js
@@ -1,21 +1,70 @@
-function greet(nombre) {
-    console.log(userName);
-    alert('¡Hola ' + userName + '! Quiero jugar un piedra, papel o tijeras contigo. ¿Crees que me vas a ganar?');
+// Declaración de tasa de cambio actualizada (Aca utilice la tasa de cambio al dia de hoy y tomando el valor del Dolar Blue y no del Dolar Oficial: 1 USD = 547 ARS)
+const exchangeRateUSDToARS = 547;
+const exchangeRateARSToUSD = 1 / exchangeRateUSDToARS;
+
+// Array para almacenar el historial de conversiones
+const conversionHistory = [];
+
+// Función para realizar la conversión de ARS a USD
+function convertARSToUSD(amountARS) {
+    const amountUSD = amountARS * exchangeRateARSToUSD;
+    return amountUSD.toFixed(2); // Redondeo a 2 decimales
 }
 
-let userName = prompt('Ingrese su nombre');
-greet();
+// Función para realizar la conversión de USD a ARS
+function convertUSDToARS(amountUSD) {
+    const amountARS = amountUSD * exchangeRateUSDToARS;
+    return amountARS.toFixed(2); // Redondeo a 2 decimales
+}
 
-for (let i = 0; i < 5; i++) {
-    let userSelect = prompt('¡Por favor, escribe tu elección! "Piedra", "Papel" o "Tijera"');
+// Función para mostrar las opciones de conversión disponibles
+function showOptions() {
+    alert("Opciones de conversión:\n1. ARS a USD\n2. USD a ARS");
+}
 
-    if (userSelect === 'Piedra') {
-        alert('¡Me ganaste ' + userName + '! Yo he elegido Tijera.');
-    } else if (userSelect === 'Papel') {
-        alert('¡Hemos empatado ' + userName + '! Yo también he elegido Papel.');
-    } else if (userSelect === 'Tijera') {
-        alert('¡Te gané ' + userName + '! Yo he elegido Piedra.');
-    } else {
-        alert(userName + ' "' + userSelect + '" ' + 'no es un elemento válido de piedra, papel o tijeras. Por favor, escribe "Piedra", "Papel" o "Tijera".');
+// Función para mostrar el historial de conversiones
+function showConversionHistory() {
+    console.log("Historial de conversiones:");
+    conversionHistory.forEach((conversion) => {
+        console.log(conversion);
+    });
+}
+
+// Función principal para el proceso de conversión
+function currencyConverter() {
+    // Mostrar las opciones de conversión disponibles
+    showOptions();
+
+    // Capturar entrada mediante prompt() para la opción seleccionada
+    const option = parseInt(prompt("Ingrese el número de opción (1 o 2):"));
+
+    // Validar la opción ingresada
+    if (option !== 1 && option !== 2) {
+        alert("Opción no válida. Por favor, ingrese 1 o 2.");
+        return;
     }
+
+    // Capturar entrada del monto a convertir mediante prompt()
+    const amount = parseFloat(prompt("Ingrese el monto a convertir:"));
+
+    // Realizar la conversión según la opción seleccionada
+    let result, conversion;
+    if (option === 1) {
+        result = convertARSToUSD(amount);
+        conversion = `${amount} ARS equivale a ${result} USD (Dólar Blue).`;
+        alert(conversion);
+    } else {
+        result = convertUSDToARS(amount);
+        conversion = `${amount} USD equivale a ${result} ARS.`;
+        alert(conversion);
+    }
+
+    // Agregar el registro al historial de conversiones
+    conversionHistory.push(conversion);
+
+    // Mostrar el historial de conversiones en la consola
+    showConversionHistory();
 }
+
+// Iniciar el proceso
+currencyConverter();
