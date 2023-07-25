84d603d9f7ba70f11860fc0b408a9960372e3ad0
Mauricio Salinas <mauriciosalinas0703@gmail.com>
2023-07-24 22:49:47 -0300

    Agregado de un bucle para realizar varias conversiones

--- js/main.js
+++ js/main.js
@@ -8,13 +8,13 @@ const conversionHistory = [];
 // Función para realizar la conversión de ARS a USD
 function convertARSToUSD(amountARS) {
     const amountUSD = amountARS * exchangeRateARSToUSD;
-    return amountUSD.toFixed(2); // Redondeo a 2 decimales
+    return amountUSD.toFixed(2); // Redondeamos a 2 decimales
 }
 
 // Función para realizar la conversión de USD a ARS
 function convertUSDToARS(amountUSD) {
     const amountARS = amountUSD * exchangeRateUSDToARS;
-    return amountARS.toFixed(2); // Redondeo a 2 decimales
+    return amountARS.toFixed(2); // Redondeamos a 2 decimales
 }
 
 // Función para mostrar las opciones de conversión disponibles
@@ -22,49 +22,59 @@ function showOptions() {
     alert("Opciones de conversión:\n1. ARS a USD\n2. USD a ARS");
 }
 
-// Función para mostrar el historial de conversiones
-function showConversionHistory() {
-    console.log("Historial de conversiones:");
-    conversionHistory.forEach((conversion) => {
-        console.log(conversion);
-    });
-}
-
 // Función principal para el proceso de conversión
 function currencyConverter() {
-    // Mostrar las opciones de conversión disponibles
-    showOptions();
+    let continueConversion = true;
 
-    // Capturar entrada mediante prompt() para la opción seleccionada
-    const option = parseInt(prompt("Ingrese el número de opción (1 o 2):"));
+    while (continueConversion) {
+        // Mostrar las opciones de conversión disponibles
+        showOptions();
 
-    // Validar la opción ingresada
-    if (option !== 1 && option !== 2) {
-        alert("Opción no válida. Por favor, ingrese 1 o 2.");
-        return;
+        // Capturar entrada mediante prompt() para la opción seleccionada
+        const option = parseInt(prompt("Ingrese el número de opción (1 o 2):"));
+
+        // Validar la opción ingresada
+        if (option !== 1 && option !== 2) {
+            alert("Opción no válida. Por favor, ingrese 1 o 2.");
+            continue;
+        }
+
+        // Capturar entrada del monto a convertir mediante prompt()
+        const amount = parseFloat(prompt("Ingrese el monto a convertir:"));
+
+        // Realizar la conversión según la opción seleccionada
+        let result, conversion;
+        if (option === 1) {
+            result = convertARSToUSD(amount);
+            conversion = `${amount} ARS equivale a ${result} USD (Dólar Blue).`;
+            alert(conversion);
+        } else {
+            result = convertUSDToARS(amount);
+            conversion = `${amount} USD equivale a ${result} ARS.`;
+            alert(conversion);
+        }
+
+        // Agregar el registro al historial de conversiones
+        conversionHistory.push(conversion);
+
+        // Preguntar al usuario si desea realizar otra conversión
+        const continueResponse = prompt("¿Desea realizar otra conversión? (Sí / No)");
+        if (continueResponse && continueResponse.toLowerCase() === "no") {
+            continueConversion = false;
+        }
     }
-
-    // Capturar entrada del monto a convertir mediante prompt()
-    const amount = parseFloat(prompt("Ingrese el monto a convertir:"));
-
-    // Realizar la conversión según la opción seleccionada
-    let result, conversion;
-    if (option === 1) {
-        result = convertARSToUSD(amount);
-        conversion = `${amount} ARS equivale a ${result} USD (Dólar Blue).`;
-        alert(conversion);
-    } else {
-        result = convertUSDToARS(amount);
-        conversion = `${amount} USD equivale a ${result} ARS.`;
-        alert(conversion);
-    }
-
-    // Agregar el registro al historial de conversiones
-    conversionHistory.push(conversion);
 
     // Mostrar el historial de conversiones en la consola
+    console.log("Historial de conversiones:");
     showConversionHistory();
 }
 
-// Iniciar el proceso
+// Función para mostrar el historial de conversiones
+function showConversionHistory() {
+    conversionHistory.forEach((conversion, index) => {
+        console.log(`Conversión ${index + 1}: ${conversion}`);
+    });
+}
+
+// Llamar a la función principal para iniciar el proceso
 currencyConverter();
