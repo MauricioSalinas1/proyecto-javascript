069b470e46ecadf9758729e23d4c425a82b028ed
Mauricio Salinas <mauriciosalinas0703@gmail.com>
2023-08-11 03:59:18 -0300

    Corrección de error de duplicacion de historial de conversiones

--- js/main.js
+++ js/main.js
@@ -36,10 +36,9 @@ function performAutoConversion() {
     const monedaA = monedaASelect.value;
     const montoDeValue = montoDeInput.value;
 
-    // Verificar si el campo de monto está vacío
     if (montoDeValue === "") {
-        updateResult(""); // Dejar el resultado vacío
-        return; // Salir de la función sin hacer la conversión
+        updateResult("");
+        return;
     }
 
     const montoDe = parseFloat(montoDeValue);
@@ -63,12 +62,41 @@ monedaASelect.addEventListener("change", performAutoConversion);
 
 // Función para agregar una conversión al historial y almacenarla en localStorage
 function addToHistoryAndLocalStorage(conversion) {
-    conversionHistory.push(conversion);
-    localStorage.setItem('conversionHistory', JSON.stringify(conversionHistory));
-    const historyItem = document.createElement("p");
-    historyItem.textContent = conversion;
-    historyItem.classList.add("historial-texto1");
-    historialSection.appendChild(historyItem);
+    if (conversion) {
+        conversionHistory.push(conversion);
+        localStorage.setItem('conversionHistory', JSON.stringify(conversionHistory));
+
+        const historyItem = document.createElement("p");
+        historyItem.classList.add("historial-texto1");
+
+        const parts = conversion.split(" = ");
+
+        const conversionInfo = parts[0];
+        const resultInfo = parts[1];
+
+        const texto1 = document.createElement("span");
+        texto1.textContent = conversionInfo;
+        texto1.classList.add("historial-texto1");
+
+        const igualSigno = document.createElement("span");
+        igualSigno.textContent = " = ";
+        igualSigno.classList.add("historial-texto1");
+
+        const texto2 = document.createElement("span");
+        texto2.textContent = resultInfo;
+        texto2.classList.add("historial-texto2");
+
+        if (conversionInfo !== "Conversión no válida.") {
+            historyItem.appendChild(texto1);
+            historyItem.appendChild(igualSigno);
+            historyItem.appendChild(texto2);
+        } else {
+            historyItem.appendChild(texto1);
+            historyItem.appendChild(texto2);
+        }
+
+        historialSection.appendChild(historyItem);
+    }
 }
 
 // Función para cargar el historial de conversiones desde localStorage al cargar la página
@@ -76,13 +104,40 @@ function loadConversionHistoryFromLocalStorage() {
     const storedHistory = localStorage.getItem('conversionHistory');
     if (storedHistory) {
         conversionHistory = JSON.parse(storedHistory);
-        conversionHistory.forEach(conversion => {
-            const historyItem = document.createElement("p");
-            historyItem.textContent = conversion;
-            historyItem.classList.add("historial-texto1");
-            historialSection.appendChild(historyItem);
-        });
+        renderConversionHistory();
     }
+}
+
+// Función para renderizar el historial de conversiones en el DOM
+function renderConversionHistory() {
+    historialSection.innerHTML = ''; // Limpiar el historial actual
+
+    conversionHistory.forEach(conversion => {
+        const historyItem = document.createElement("p");
+        historyItem.classList.add("historial-texto1");
+
+        const parts = conversion.split(" = ");
+        const conversionInfo = parts[0];
+        const resultInfo = parts[1];
+
+        const texto1 = document.createElement("span");
+        texto1.textContent = conversionInfo;
+        texto1.classList.add("historial-texto1");
+
+        const igualSigno = document.createElement("span");
+        igualSigno.textContent = " = ";
+        igualSigno.classList.add("historial-texto1");
+
+        const texto2 = document.createElement("span");
+        texto2.textContent = resultInfo;
+        texto2.classList.add("historial-texto2");
+
+        historyItem.appendChild(texto1);
+        historyItem.appendChild(igualSigno);
+        historyItem.appendChild(texto2);
+
+        historialSection.appendChild(historyItem);
+    });
 }
 
 // Función para intercambiar las monedas seleccionadas
@@ -93,7 +148,7 @@ function swapCurrencies() {
     monedaDeSelect.value = moneda2;
     monedaASelect.value = moneda1;
 
-    performAutoConversion(); // Actualizar la conversión automáticamente
+    performAutoConversion();
 }
 
 // Asociar la función de intercambio al evento click del botón "Intercambiar monedas"
@@ -101,8 +156,8 @@ swapButton.addEventListener("click", swapCurrencies);
 
 // Función para borrar el historial de conversiones y removerlo de localStorage
 function clearConversionHistory() {
-    historialSection.innerHTML = ""; // Limpiar el contenido del elemento
-    conversionHistory = []; // Vaciar el array
+    historialSection.innerHTML = "";
+    conversionHistory = [];
     localStorage.removeItem('conversionHistory');
 }
 
@@ -116,20 +171,22 @@ swapButton.addEventListener("click", swapCurrencies);
 // Al cargar la página, cargar el historial de conversiones desde localStorage
 window.addEventListener("load", loadConversionHistoryFromLocalStorage);
 
-// Función para guardar la conversión manualmente
+/// Función para guardar la conversión manualmente
 function saveConversion() {
     const monedaDe = monedaDeSelect.value;
     const monedaA = monedaASelect.value;
     const montoDe = parseFloat(montoDeInput.value);
 
     let result, conversion;
-    if (isNaN(montoDe)) {
-        conversion = "Conversión inválida: Ingresa un monto numérico.";
-    } else if (exchangeRates.hasOwnProperty(monedaDe) && exchangeRates.hasOwnProperty(monedaA)) {
+    if (!monedaDe || !monedaA) {
+        conversion = "Moneda inválida: Debes elegir ambas monedas.";
+    } else if (isNaN(montoDe)) {
+        conversion = "Monto inválido: Debes ingresar un monto numérico.";
+    } else if (!exchangeRates.hasOwnProperty(monedaDe) || !exchangeRates.hasOwnProperty(monedaA)) {
+        conversion = "Conversión no válida: Moneda(s) no soportada(s).";
+    } else {
         result = (montoDe / exchangeRates[monedaDe] * exchangeRates[monedaA]).toFixed(2);
         conversion = `${montoDe} ${monedaDe} = ${result} ${monedaA}.`;
-    } else {
-        conversion = "Conversión no válida.";
     }
 
     updateResult(result);
